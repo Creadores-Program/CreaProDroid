2df36106de99c424ca5cfcd09467d13db8b5f48e
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-06-28 08:47:10 -0600

    Update MaxIaManager.java

--- app/src/main/java/org/CreadoresProgram/CreaProDroid/IA/MaxIaManager.java
+++ app/src/main/java/org/CreadoresProgram/CreaProDroid/IA/MaxIaManager.java
@@ -44,8 +44,9 @@ public class MaxIaManager{
         .readTimeout(120, TimeUnit.SECONDS)
         .connectionSpecs(Collections.singletonList(
             new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
-                .tlsVersions(TlsVersion.TLS_1_2)
-                .build()
+                .tlsVersions(TlsVersion.TLS_1_3, TlsVersion.TLS_1_2)
+                .build(),
+            ConnectionSpec.COMPATIBLE_TLS
         ))
         .build();
     private static final MediaType JSONHt = MediaType.parse("application/json; charset=utf-8");
