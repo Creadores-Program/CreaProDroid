949a8dffcead3dd62561729422b73b47c94c1670
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-07-21 13:05:15 -0600

    Refine paragraph pattern in markdown-to-html conversion

--- app/src/main/assets/libsJs/markdown-to-html-cpd-edition.js
+++ app/src/main/assets/libsJs/markdown-to-html-cpd-edition.js
@@ -84,7 +84,7 @@ var
         {
             // <p>
             // Paragraph
-            pattern: /^(?!<\/?\w+>|\s?\*|\s?[0-9]+|>|\&gt;|-{3,})([^\n]+)$/gm,
+            pattern: /^(?!<\/?(?:w+|pre|code|textarea)[^>]*>|\s?\*|\s?[0-9]+|>|\&gt;|-{3,}|[\s\S]*?<\/pre>)(.+)$/gm,
             replace: "<p>$1</p>",
             type: BLOCK,
         },
