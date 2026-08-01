11bd1835308d63338290af4cddc6d484c6882725
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-08-01 14:15:13 -0600

    Create CreaLink.java

---
+++ app/src/main/java/org/CreadoresProgram/CreaProDroid/IA/Plugins/CreaLink.java
@@ -0,0 +1,11 @@
+package org.CreadoresProgram.CreaProDroid.IA.Plugins;
+import android.content.Context;
+public class CreaLink extends PluginIA {
+    public AvancedActions(Context context){
+        super(context);
+    }
+    @Override
+    public String getInfo(){
+        return "[CreaLink]\n**si el plugin AvancedActions esta desactivado ignora este plugin**\nEn la opcion url de tu json de respuesta puedes usar estos intents para conectar con apps de Creadores Program (si estan instaladas)\n- CreaTV\n  - package: org.CreadoresProgram.CreaTv\n  - formato de intent: para abrir un live o stream de Twitch, Kick, YouTube o TikTok puedes abrir CreaTV con la clase org.CreadoresProgram.CreaTv.StreamActivity, con data del url y algunos extras!, extras que existen:\n    - org.CreadoresProgram.CreaTv.QUALITY: calidad del stream (solamente hay 'link_worst' y 'link_best')\n    - org.CreadoresProgram.CreaTv.ONCHAT: para abrir el chat (solo disponible para streams de Twitch)\n  - Ejemplo de intent: intent://twitch.tv/trollhunters501#Intent;scheme=https;package=org.CreadoresProgram.CreaTv;component=org.CreadoresProgram.CreaTv/.StreamActivity;end\nUsa los intents si el usuario tiene las apps anteriores";
+    }
+}
